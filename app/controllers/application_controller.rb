c11b62c86dca555bfea5c48c16cfa573645cb83a
Ruben Voss <bekertshop@gmail.com>
2022-08-22 16:28:19 +0200

    added comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+  # check if the user is logged in before any action:
   before_action :authenticate_user!
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  # don't require login on the homepage:
   skip_before_action :authenticate_user!, only: :home
 
   def home
