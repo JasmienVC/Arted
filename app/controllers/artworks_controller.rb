5131e7bfd433fe7820e85c98615fc1e06d012e0b
Ruben Voss <bekertshop@gmail.com>
2022-08-23 11:01:01 +0200

    made artworks controller

---
+++ app/controllers/artworks_controller.rb
@@ -0,0 +1,22 @@
+class ArtworksController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
