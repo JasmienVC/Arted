db9e7b798cffd378f606948f65510a07b911c84b
Ruben Voss <bekertshop@gmail.com>
2022-08-23 11:27:11 +0200

    added resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Rails.application.routes.draw do
-  get 'transactions/index'
-  get 'transactions/show'
-  get 'profiles/show'
-  get 'profiles/edit'
-  get 'profiles/update'
-  get 'profiles/destroy'
-  get 'artworks/show'
-  get 'artworks/create'
-  get 'artworks/new'
-  get 'artworks/index'
-  get 'artworks/edit'
-  get 'artworks/update'
-  get 'artworks/destroy'
+
+  resources :artworks
+  resources :profiles
+  resources :transactions
+
+
+  # get 'transactions/index'
+  # get 'transactions/show'
+  # get 'profiles/show'
+  # get 'profiles/edit'
+  # get 'profiles/update'
+  # get 'profiles/destroy'
+  # get 'artworks/show'
+  # get 'artworks/create'
+  # get 'artworks/new'
+  # get 'artworks/index'
+  # get 'artworks/edit'
+  # get 'artworks/update'
+  # get 'artworks/destroy'
   root to: "pages#home"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
