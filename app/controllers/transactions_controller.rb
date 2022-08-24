51208f5d003e34eaab287bb4fff767ef8c4d10ba
Ruben Voss <bekertshop@gmail.com>
2022-08-24 12:15:40 +0200

    added new and create actions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,4 +4,19 @@ class TransactionsController < ApplicationController
 
   def show
   end
+
+  def new
+    @transaction = Transaction.new
+  end
+
+  def create
+    # @transaction = Transaction.new(add the user_id, artwork_id, status)
+    @transaction = Transaction.new
+
+    if @transaction.save
+      redirect_to transaction_path(@transaction)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
