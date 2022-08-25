b5ff1c0226c8a560221f452e8922161759048c7d
Jasmien Van Cleuvenbergen <jasmien.vancleuvenbergen@gmail.com>
2022-08-25 11:00:22 +0200

    search function works in rails c

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -3,4 +3,6 @@ class Artwork < ApplicationRecord
   belongs_to :user
   has_one :artwork_transaction
   has_many_attached :photos
+  include PgSearch::Model
+  multisearchable against: %i[title description user]
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+  include PgSearch::Model
+  multisearchable against: %i[name]
 end
