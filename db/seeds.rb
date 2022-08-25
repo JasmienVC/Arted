d17553d66e67cd77cc9a374889aaaa42a15394e2
Ruben Voss <bekertshop@gmail.com>
2022-08-25 08:54:48 +0200

    better every time

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,8 +52,8 @@ def make_random_artwork(random_user_id, artwork_photo)
   artwork = Artwork.new(title: Faker::Emotion.noun.capitalize, description: Faker::Lorem.paragraph(sentence_count: 5))
   artwork.photos.attach(io: artwork_photo, filename: artwork.title, content_type: "image/jpg")
   artwork.user_id = random_user_id
-  artwork.price = 950
-  artwork.shipping_costs = 2.99
+  artwork.price = Faker::Number.between(from: 400, to: 3000)
+  artwork.shipping_costs = 9.99
   artwork.save
   puts "user_id: #{artwork.user_id} - Artwork created: #{artwork.title}  - saved?: #{artwork.save} - id: #{artwork.id}"
   puts ""
