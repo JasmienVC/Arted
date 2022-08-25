762d229228a30d32983e21bd33baa7cd14673a32
Ruben Voss <bekertshop@gmail.com>
2022-08-25 08:31:44 +0200

    working on the refactoring... added text in the terminal

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ puts "cleaning database...."
 Profile.destroy_all
 Artwork.destroy_all
 User.destroy_all
-Transaction.destroy_all
+ArtworkTransaction.destroy_all
 
 puts "making admin user..."
 admin = User.new
@@ -14,11 +14,11 @@ admin.password = "123456"
 admin.save!
 
 ######### Artworks seeds:
-artworks = ["https://res.cloudinary.com/dlpbxzb7o/image/upload/v1661330810/seeds/7g14doghnwc1uh2sg0xfz837ftfd.jpg",
+artworks_photos = ["https://res.cloudinary.com/dlpbxzb7o/image/upload/v1661330810/seeds/7g14doghnwc1uh2sg0xfz837ftfd.jpg",
             "https://res.cloudinary.com/dlpbxzb7o/image/upload/v1661330814/seeds/r2vdzjwg4bxvesi8zhxu8q4sw526.jpg",
             "https://res.cloudinary.com/dlpbxzb7o/image/upload/v1661330817/seeds/9mrxd8ku47yurs4vc0dxmdl6d0u7.jpg",
             "https://res.cloudinary.com/dlpbxzb7o/image/upload/v1661330819/seeds/f1wkf2jp1xi17bb3nwgc33en5ycm.jpg",
-            "https://res.cloudinary.com/dlpbxzb7o/image/upload/v1661330822/seeds/tv4m6nrqg1wssnhm5rkbu2o06y7p.jpg"]
+                   "https://res.cloudinary.com/dlpbxzb7o/image/upload/v1661330822/seeds/tv4m6nrqg1wssnhm5rkbu2o06y7p.jpg"]
 
 artists_photos = ["https://res.cloudinary.com/dlpbxzb7o/image/upload/v1661330812/seeds/ur89m1htbvwb2jutxt19v61cppuf.png",
                   "https://res.cloudinary.com/dlpbxzb7o/image/upload/v1661330815/seeds/ojf4fvzyq9mpfepr6hs9htmtqb2e.jpg",
@@ -30,33 +30,43 @@ def make_random_user
   # makes a new random user and returns the user
   user = User.new
   user.email = Faker::Internet.email
-  user.password = '#$taawktljasktlw4aaglj'
-  user.save!
+  user.password = '123456'
+  user.save
+  puts "User created: #{user.email} - #{user.password} - saved?: #{user.save}"
   user
 end
 
-puts "populating database..."
-artworks.each_with_index do |artwork, index|
-  random_user_id = make_random_user.id
+def make_random_artist_profile(random_user_id, artist_photo)
+  # find the artists profile, because it was already created at user creation
+  artist_profile = Profile.find(random_user_id)
+  artist_profile.name = "coolerKünster#{random_user_id}"
+  artist_profile.bio = "lalalalalalal lorem ipsum dolor"
+  artist_profile.photo.attach(io: artist_photo, filename: "profilepic.jpg", content_type: "image/jpg")
+  artist_profile.user_id = random_user_id
+  artist_profile.save
+  puts "ArtistProfile created: #{artist_profile.name} - user_id: #{artist_profile.user_id} - saved?: #{artist_profile.save}"
+end
 
-
-  # add a artwork price and shipping
-  artwork_photo = URI.open(artwork)
+def make_random_artwork(random_user_id, artwork_photo)
   artwork = Artwork.new(title: "ART", description: "this is the description of this cool artwork")
   artwork.photos.attach(io: artwork_photo, filename: "art.jpg", content_type: "image/jpg")
   artwork.user_id = random_user_id
   artwork.price = 950
   artwork.shipping_costs = 2.99
   artwork.save
+end
 
-  artist_photo = URI.open(artists_photos[index])
+puts "populating database..."
+n = 0
+5.times do
+  random_user_id = make_random_user.id
+  p "random_user_id: #{random_user_id}"
 
-  # find the artists profile, because it was already created at user creation
+  artwork_photo = URI.open(artworks_photos[n])
+  artist_photo = URI.open(artists_photos[n])
 
-  artist_profile = Profile.find(index + 1)
-  artist_profile.name = "coolerKünster#{index + 1}"
-  artist_profile.bio = "lalalalalalal lorem ipsum dolor"
-  artist_profile.photo.attach(io: artist_photo, filename: "profilepic.jpg", content_type: "image/jpg")
-  artist_profile.user_id = random_user_id
-  artist_profile.save
+  make_random_artist_profile(random_user_id, artist_photo)
+  make_random_artwork(random_user_id, artwork_photo)
+
+  n += 1
 end
