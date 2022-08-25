13bb4a3b1bd779000fc615666c7ea1a08cfd05b5
Ruben Voss <bekertshop@gmail.com>
2022-08-25 14:44:53 +0200

    added some connections between tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :artworks
-  has_many :transactions
-  has_one :profile
+  has_many :artwork_transactions, dependent: :destroy
+  has_many :artworks, dependent: :destroy
+  has_one :profile, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   after_create :create_profile
